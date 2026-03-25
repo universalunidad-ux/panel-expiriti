32f64afab13dc97af763767df2c81e863bf0d3b4
universalunidad-ux <universalunidad@gmail.com>
2026-03-25 02:59:26 -0600

    Update supabase.js

--- PANEL/supabase.js
+++ PANEL/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'AQUI_TU_URL'
-const supabaseKey = 'AQUI_TU_ANON_KEY'
+const supabaseUrl = 'https://ovfmqqqwezfdtgrtkjhf.supabase.co'
+const supabaseKey = 'sb_publishable_2ftu336Kc06w2I2iTwoIpQ_usfSTNG9'
 
 // ⚠️ pega tus valores reales desde Settings → API
 export const supabase = createClient(supabaseUrl, supabaseKey)
